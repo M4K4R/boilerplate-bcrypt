771aecd034a184c155919e43b90c7925091df903
M4K4R <37383837+M4K4R@users.noreply.github.com>
2022-02-10 14:28:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res); //true
   });
 });
+//END_ASYNC
+
+//START_SYNC
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+//END_SYNC
 
 
 app.listen(process.env.PORT || 3000, () => {});
